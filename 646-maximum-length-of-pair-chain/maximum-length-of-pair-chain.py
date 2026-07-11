77badb7f0ced877de1775cbed48ba8e00b53312e
Subhan Asghar <subhanasghar16@gmail.com>
2026-07-11 12:29:23 -0700

    Time: 815 ms (26.34%) | Memory: 19.7 MB (33.14%) - LeetSync

--- 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.py
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.py
@@ -1,17 +1,24 @@
 class Solution:
     def findLongestChain(self, pairs: List[List[int]]) -> int:
         pairs.sort()
-        def func(index,prev):
-            if index==len(pairs):
-                return 0
-            if (index,prev) in memo:
-                return memo[(index,prev)]
-            not_take=func(index+1,prev)
-            take=0
-            if prev==-1 or pairs[index][0]>pairs[prev][1]:
-                take= 1+func(index+1,index)
-            ans=max(take,not_take)
-            memo[(index,prev)]=ans
-            return ans
-        memo={}
-        return func(0,-1)
+        # def func(index,prev):
+        #     if index==len(pairs):
+        #         return 0
+        #     if (index,prev) in memo:
+        #         return memo[(index,prev)]
+        #     not_take=func(index+1,prev)
+        #     take=0
+        #     if prev==-1 or pairs[index][0]>pairs[prev][1]:
+        #         take= 1+func(index+1,index)
+        #     ans=max(take,not_take)
+        #     memo[(index,prev)]=ans
+        #     return ans
+        # memo={}
+        # return func(0,-1)
+        n=len(pairs)
+        dp=[1]*n
+        for index in range(1,n):
+            for prev in range(index):
+                if pairs[index][0]>pairs[prev][1]:
+                    dp[index]=max(dp[index],dp[prev]+1)
+        return max(dp)
