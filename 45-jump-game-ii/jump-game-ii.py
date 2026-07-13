a5c9309f9cbea0ee6f98ed861837a1d392e8c190
Subhan Asghar <subhanasghar16@gmail.com>
2026-07-13 05:07:00 -0700

    Time: 2659 ms (11.27%) | Memory: 20.1 MB (34.10%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.py
+++ 45-jump-game-ii/jump-game-ii.py
@@ -1,16 +1,24 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        memo={}
-        def func(index):
-            if index==len(nums)-1:
-                return 0
-            if index in memo:
-                return memo[index]
-            ans=float('inf')
-            for i in range(1,nums[index]+1):
-                if index+i>=len(nums):
-                    continue
-                ans=min(ans,func(index+i)+1)
-            memo[index]=ans
-            return ans
-        return func(0)
+        # memo={}
+        # def func(index):
+        #     if index==len(nums)-1:
+        #         return 0
+        #     if index in memo:
+        #         return memo[index]
+        #     ans=float('inf')
+        #     for i in range(1,nums[index]+1):
+        #         if index+i>=len(nums):
+        #             continue
+        #         ans=min(ans,func(index+i)+1)
+        #     memo[index]=ans
+        #     return ans
+        # return func(0)
+        n=len(nums)
+        dp=[float('inf')]*n
+        dp[n-1]=0
+        for i in range(n-2,-1,-1):
+            for jump in range(1,nums[i]+1):
+                if i+jump<n:
+                    dp[i]=min(dp[i],dp[jump+i]+1)
+        return dp[0]
