a6d28dba59e8c5a827c40195f874cadb1d80c274
Subhan Asghar <subhanasghar16@gmail.com>
2026-07-19 15:33:54 -0700

    Time: 11 ms (99.88%) | Memory: 25.1 MB (84.41%) - LeetSync

--- 491-non-decreasing-subsequences/non-decreasing-subsequences.py
+++ 491-non-decreasing-subsequences/non-decreasing-subsequences.py
@@ -3,11 +3,15 @@ class Solution:
         result=[]
         n=len(nums)
         def backtrack(index,path):
-            if len(path)>1 and path not in result:
+            if len(path)>1:
                 result.append(path[:])
+            used=set()
             for i in range(index,n):
+                if nums[i] in used:
+                    continue
                 if path and path[-1]>nums[i]:
                     continue
+                used.add(nums[i])
                 path.append(nums[i])
                 backtrack(i+1,path)
                 path.pop()
