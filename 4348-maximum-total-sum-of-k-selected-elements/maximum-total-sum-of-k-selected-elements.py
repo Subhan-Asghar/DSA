8aef19dc519694a15bc3f83fba3bb62c23297b73
Subhan Asghar <subhanasghar16@gmail.com>
2026-07-21 04:19:07 -0700

    Time: 184 ms (75.53%) | Memory: 34.2 MB (92.11%) - LeetSync

--- 4348-maximum-total-sum-of-k-selected-elements/maximum-total-sum-of-k-selected-elements.py
+++ 4348-maximum-total-sum-of-k-selected-elements/maximum-total-sum-of-k-selected-elements.py
@@ -1,11 +1,10 @@
 import heapq as hq
 class Solution:
     def maxSum(self, nums: list[int], k: int, mul: int) -> int:
-        heap=[-x for x in nums]
-        hq.heapify(heap)
+        nums.sort(reverse=True)
         total_sum=0
-        for _ in range(k):
-            val=-hq.heappop(heap)
+        for i in range(k):
+            val=nums[i]
             if mul>0:
                 total_sum+=val*mul
                 mul-=1
